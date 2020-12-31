2c9013a209ec8d6d5b4ebadc9e7d37b0c5644f96
Novikevicius <edvardas.novikevicius@gmail.com>
2020-12-31 16:01:29 +0200

    implemented Input class and map unit test

--- QTable.py
+++ QTable.py
@@ -13,77 +13,29 @@ class QTable(object):
         return s
 
 class Input(object):
-    def __init__(self, action_space) -> None:
-        self.action_space = action_space
-        b1, b2 = Bucket(self.action_space, float('-inf'), float('inf')).split()
-        self.buckets = [b1, b2]
-    def train(self, env, epochs=100, lr=0.01):
-        pass
-    def get_values(self, state):
-        for b in self.buckets:
-            if b.is_in_range:
-                return b
-        return None
-    def get_max_value(self, state):
-        return self.get_value(state).get_max()
-    def get_best_action(self, state):
-        pass #return
-    def split(self, i, step=100):
-        if i < 0 or i > len(self.buckets):
-            return
-        b1, b2 = self.buckets[i].split(step)
-        a1 = self.buckets[:(i-1)]
-        a2 = self.buckets[(i+1):]
-        self.buckets = a1
-        self.buckets.append(b1)
-        self.buckets.append(b2)
-        self.buckets.extend(a2)
-    def __str__(self):
-        s = "["
-        for b in self.buckets:
-            s += '{' + str(b) + "},\n"
-        s += ']'
-        return s
-class Bucket(object):
-    # [min, max)
-    def __init__(self, space, min, max, isTraining=True):
-        self.space = space
+    def __init__(self, min, max, step_size, precision=2) -> None:
+        self.precision = precision
         self.min = min
         self.max = max
-        self.values = np.zeros(self.space)
-        self.occurences = 0
-    def get_value(self, index):
-        if index < 0 or index > self.space:
-            return
-        return self.values[index]
-    def get_max(self):
-        return (max(self.values), argmax(self.values))
-    def set_value(self, index, value):
-        if index < 0 or index > self.space:
-            return
-        if value < min or value >= max:
-            return
-        self.values[index] = value
-    def resize(self, min, max):
-        self.min = min
-        self.max = max
-    def split(self, step=100):
-        min = self.min
-        max = self.max
-        if min == float('-inf') and max == float('inf'):
-            mid = 0
-        elif min == float('-inf'):
-            mid = max - step
-        elif max == float('inf'):
-            mid = min + step
-        else:            
-            mid = (self.max+self.min)/2
-        b1 = Bucket(self.space, min, mid)
-        b1.values = np.copy(self.values)
-        b2 = Bucket(self.space, mid, max)
-        b2.values = np.copy(self.values)
-        return (b1, b2)
-    def is_in_range(self, value):
-        return value >= self.min and value < self.max
-    def __str__(self):
-        return str.format("[{0}, {1}) ", self.min, self.max) + str(self.values)
+        self.step_size = step_size
+        self.size = int((self.max-self.min+1) / step_size + 2) # -inf & +inf
+        self.values = [0] * self.size
+    def split(self, step_size):
+        return (self.max - self.min) / step_size
+    def map(self, value):
+        if( value <= self.min):
+            return 0
+        if (value >= self.max):
+            return self.size-1
+        return (round(value, self.precision)-self.min) * self.step_size + 1
+
+
+def test_map():
+    min = 1
+    max = 5
+    step = 1
+    input = Input(min, max, step, 0)
+    assert(input.map(min) == 0)
+    assert(input.map(max) == input.size-1)
+    for i in range(min+1, max-1, step):
+        assert(input.map(i) == i)
