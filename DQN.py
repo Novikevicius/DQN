689bdacde16101b4bd9839b22fe0b1c548a11ca1
Novikevicius <edvardas.novikevicius@gmail.com>
2021-01-11 17:50:10 +0200

    finished CartPole & FrozenLake experiments

--- DQN.py
+++ DQN.py
@@ -199,22 +199,45 @@ def run_frozen_lake_experiments():
             f.close()
     else:
         ID = 0
-    ID = run_frozen_lake_experiment(ID, epochs=3000, lr=0.1, gamma=0.99)
-    ID = run_frozen_lake_experiment(ID, epochs=3000, lr=0.01, gamma=0.99)
-    ID = run_frozen_lake_experiment(ID, epochs=3000, lr=0.1, gamma=0.99)
-    ID = run_frozen_lake_experiment(ID, epochs=10000, lr=0.1, gamma=0.99, result_x_size=500)
+    global env    
+    env = gym.make("FrozenLake-v0")
+    ID = run_frozen_lake_experiment(ID, epochs=10000, lr=0.01, gamma=0.99, result_x_size=1000)
+    ID = run_frozen_lake_experiment(ID, epochs=30000, lr=0.01, gamma=0.99, result_x_size=1000)
+    ID = run_frozen_lake_experiment(ID, epochs=30000, lr=0.1,  gamma=0.99, result_x_size=1000)
+    ID = run_frozen_lake_experiment(ID, epochs=30000, lr=0.1,  gamma=0.99, result_x_size=1000)
+    ID = run_frozen_lake_experiment(ID, epochs=30000, lr=0.1,  gamma=0.99, result_x_size=1000)
     with open(experiment_ID_file, 'w') as f:
         f.write(str(ID)+'\n')
         f.close()
-
+def run_cartpole_experiments():
+    global MODELS_FOLDER
+    MODELS_FOLDER = 'experiments/Q_Table/CartPole/models/'
+    experiment_ID_file = 'experiments/Q_Table/CartPole/exp_ID.txt'
+    if os.path.exists(experiment_ID_file):
+        with open(experiment_ID_file, 'r+') as f:
+            ID = int(f.readline())
+            f.close()
+    else:
+        ID = 0
+    global env
+    env = gym.make("CartPole-v0")
+    ID = run_cartpole_experiment(ID, epochs=100000, lr=0.5,  gamma=0.99, result_x_size=1000)
+    ID = run_cartpole_experiment(ID, epochs=100000, lr=0.7,  gamma=0.99, result_x_size=1000)
+    #ID = run_cartpole_experiment(ID, epochs=30000, lr=0.1,  gamma=0.99, result_x_size=1000)
+    #ID = run_cartpole_experiment(ID, epochs=30000, lr=0.01, gamma=0.99, result_x_size=1000)
+    #ID = run_cartpole_experiment(ID, epochs=30000, lr=0.01, gamma=0.99, result_x_size=1000)
+    #ID = run_cartpole_experiment(ID, epochs=30000, lr=0.1,  gamma=0.99, result_x_size=1000)
+    with open(experiment_ID_file, 'w') as f:
+        f.write(str(ID)+'\n')
+        f.close()
 def run_frozen_lake_experiment(ID, epochs=100, lr=0.01, gamma=0.99, result_x_size=100):
+    global env
     def choose_action(table, state):
         if random.uniform(0, 1) > epsilon:
             action = np.argmax(table.getValue(state))
         else:
             action = env.action_space.sample()
         return action
-    global env
     experiments_folder = 'experiments'
     agent_folder    = 'Q_Table/FrozenLake'
     folder          = experiments_folder + '/' + agent_folder
@@ -232,11 +255,10 @@ def run_frozen_lake_experiment(ID, epochs=100, lr=0.01, gamma=0.99, result_x_siz
     max_exploration_rate = 1
     min_exploration_rate = 0.01
     exploration_decay_rate = 0.01
-    #table = QTable(env.observation_space.shape[0], env.action_space.n)
-    model = [QTable.Input(0, 16, 1, 0)]
+    model = [QTable.Input(0, 15, 1, 0)]
     table = QTable.QTable(env.action_space.n, model=model)
     rewards = []
-    max_steps = 100
+    max_steps = 200
     for e in range(epochs):
         state = env.reset()
         done = False
@@ -244,8 +266,10 @@ def run_frozen_lake_experiment(ID, epochs=100, lr=0.01, gamma=0.99, result_x_siz
         for s in range(max_steps):
             action = choose_action(table, state)
             new_state, reward, done, _ = env.step(action)
+            v = table.getValue(state)
             q_new = table.getValue(state)[action] * (1-lr) + lr * (reward + gamma * np.max(table.getValue(new_state)))
             table.setValue(state, action, q_new)
+            #table.setValue(state, action, q_new, e < 100)
             state = new_state
             r += reward
             if done:
@@ -266,6 +290,78 @@ def run_frozen_lake_experiment(ID, epochs=100, lr=0.01, gamma=0.99, result_x_siz
         f.write("Epochs: "        + str(epochs)    + '\n')
         f.write("Learning rate: " + str(lr)        + '\n')
         f.write("Gamma: "         + str(gamma)     + '\n')
+        f.write("Last Reward: "   + str(rewards[len(rewards)-1])     + '\n')
+
+        plot(results, fullPath, ID, xs=[i for i in range(result_x_size, epochs+1, result_x_size)])
+        f.write("Final score: " + str(results[len(results)-1]) + '\n')
+        print("Final score: " + str(results[len(results)-1]))
+    table.save(MODELS_FOLDER+str(ID))
+    return ID+1
+
+def run_cartpole_experiment(ID, epochs=100, lr=0.01, gamma=0.99, result_x_size=100):
+    def choose_action(table, state):
+        if random.uniform(0, 1) > epsilon:
+            action = np.argmax(table.getValue(state))
+        else:
+            action = env.action_space.sample()
+        return action
+    global env
+    experiments_folder = 'experiments'
+    agent_folder    = 'Q_Table/CartPole'
+    folder          = experiments_folder + '/' + agent_folder
+    file            = 'CartPole_'+str(ID)
+    fullPath        = folder + '/' + file
+    fullPathWithExt = fullPath + '.txt'
+
+    if not os.path.exists(experiments_folder):
+        os.mkdir(experiments_folder)
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+
+    print("Running experiment " + str(ID) + ":")
+    epsilon = 1
+    max_exploration_rate = 1
+    min_exploration_rate = 0.01
+    exploration_decay_rate = 0.01
+    model = [QTable.Input(-1, 1, 0.1, 4), 
+             QTable.Input(-1, 1, 0.1, 4),
+             QTable.Input(-1, 1, 0.1, 4),
+             QTable.Input(-1, 1, 0.1, 4)]
+    import DQTable as dqt
+    table = dqt.DQTable(env.action_space.n, model=model)
+    rewards = []
+    max_steps = 500
+    for e in range(epochs):
+        state = env.reset()
+        done = False
+        r = 0
+        for s in range(max_steps):
+            action = choose_action(table, state)
+            new_state, reward, done, _ = env.step(action)
+            q_new = table.getValue(state)[action] * (1-lr) + lr * (reward + gamma * np.max(table.getValue(new_state)))
+            table.setValue(state, action, q_new)
+            #table.setValue(state, action, q_new, e < 100)
+            state = new_state
+            r += reward
+            if done:
+                break
+        epsilon = min_exploration_rate + (max_exploration_rate - min_exploration_rate) * np.exp(-exploration_decay_rate*e)
+        rewards.append(r)
+    
+    rewards_per_x_episodes = np.split(np.array(rewards),epochs/result_x_size)
+    count = result_x_size
+
+    results = [] # average rewards per result_x_size episodes
+    for r in rewards_per_x_episodes:
+        results.append(sum(r/result_x_size))
+        count += result_x_size
+    
+    with open(fullPathWithExt, 'w') as f:
+        f.write("Experiment "     + str(ID)        + ':\n')
+        f.write("Epochs: "        + str(epochs)    + '\n')
+        f.write("Learning rate: " + str(lr)        + '\n')
+        f.write("Gamma: "         + str(gamma)     + '\n')
+        f.write("Last Reward: "   + str(rewards[len(rewards)-1])     + '\n')
 
         plot(results, fullPath, ID, xs=[i for i in range(result_x_size, epochs+1, result_x_size)])
         f.write("Final score: " + str(results[len(results)-1]) + '\n')
@@ -352,14 +448,35 @@ def run_experiment(ID, epochs = 100, lr=0.01, gamma=0.99, activation='linear', l
 if __name__ == "__main__":
     import sys
     global env
-    #env = gym.make("CartPole-v1")
-    env = gym.make("FrozenLake-v0")
+    env = gym.make("CartPole-v1")
+    #env = gym.make("FrozenLake-v0")
    
-    agent = load_frozen_lake_agent(16)
-    play_frozen_lake(agent, n=1000, verbose=3)
+    '''
+    model = [QTable.Input(0, 1, 1, 0), QTable.Input(1, 2, 1, 0)]
+    import DQTable as dqt
+    table = dqt.DQTable(4, model=model)
+    for a in range(4):
+        table.setValue([-1, 0], a, (a+1))
+    for a in range(4):
+        table.setValue([1, 0], a, 2*(a+1))
+    for a in range(4):
+        table.setValue([2, 0], a, 3*(a+1))
+        
+    table.getValue([1, 0], True)
+    for e in range(20):
+        state = env.reset()
+        for s in range(100):
+            action = env.action_space.sample()
+            table.getValue([2, 0], True)
+            state, r, done, info = env.step(action)
+    print(table)'''
+    #agent = load_frozen_lake_agent(16)
+    #play_frozen_lake(agent, n=1000, verbose=3)
     #run()
     #run_DQN_experiments()
     #run_frozen_lake_experiments()
+    run_cartpole_experiments()
+    #run_cartpole_experiments()
 
     env.close()
     sys.exit(0)
